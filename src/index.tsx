418d418a8b88a16a6b720a0dc8c9b7d2120030b8
Takenoko <takenoko.tech@gmail.com>
2019-02-13 02:04:23 +0900

    add panda

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import GLTFLoader from "three-gltf-loader";
+import OrbitControls from "three-orbitcontrols";
 
 // 幅、高さ取得
 const width = window.innerWidth;
@@ -18,19 +19,46 @@ camera.position.set(0, 1, 5);
 const light = new THREE.AmbientLight(0xffffff, 1);
 scene.add(light);
 
-// メッシュの作成と追加
-const grid = new THREE.GridHelper(10, 5);
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1),
-    new THREE.MeshPhongMaterial({ color: 0x0074df })
-);
-sphere.position.set(0, 1, 0);
-scene.add(grid, sphere);
+// OrbitControls の追加
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.userPan = false;
+controls.userPanSpeed = 0.0;
+controls.maxDistance = 5000.0;
+controls.maxPolarAngle = Math.PI * 0.495;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 1.0;
+
+// // メッシュの作成と追加
+// const grid = new THREE.GridHelper(10, 5);
+// const sphere = new THREE.Mesh(
+//     new THREE.SphereGeometry(1),
+//     new THREE.MeshPhongMaterial({ color: 0x0074df })
+// );
+// sphere.position.set(0, 1, 0);
+// scene.add(grid, sphere);
+
+new GLTFLoader().load("model/panda.gltf", data => {
+    const gltf = data;
+    const object = gltf.scene;
+    const animations = gltf.animations;
+
+    if (animations && animations.length) {
+        mixer = new THREE.AnimationMixer(object);
+        for (let anim of animations) {
+            mixer.clipAction(anim).play();
+        }
+    }
+
+    scene.add(object);
+});
 
 // レンダリング
 const animation = () => {
     renderer.render(scene, camera);
+    mixer && mixer.update(clock.getDelta());
     requestAnimationFrame(animation);
 };
 
+const clock = new THREE.Clock();
+let mixer: THREE.AnimationMixer;
 animation();
---
+++ src/mix.tsx
@@ -0,0 +1,5 @@
+import * as THREE from "three";
+import GLTFLoader from "three-gltf-loader";
+import OrbitControls from "three-orbitcontrols";
+
+export default {};
