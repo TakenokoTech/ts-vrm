ce4c10af750574cc0a98587a818829f0d8062b1c
Takenoko <takenoko.tech@gmail.com>
2019-03-06 00:57:22 +0900

    fix declare

--- react-vrm/types.d.ts
+++ react-vrm/types.d.ts
@@ -1,8 +1,10 @@
+declare module "three-orbitcontrols";
+declare module "stats-js";
+
+declare var window: Window;
 interface Window {
     changeBoneAngle: (axis: string, value: number) => void;
     changeExpression: (value: number) => void;
 }
 
-declare var window: Window;
-declare module "three-orbitcontrols";
-declare module "stats-js";
+declare class OrbitControls {}
